d24875bddefd001b683400c9b1320b515d255495
hendisantika <hendisantika@yahoo.co.id>
2019-02-28 07:14:02 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootsftpdownloaddemo/SpringbootSftpDownloadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsftpdownloaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSftpDownloadDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSftpDownloadDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootsftpdownloaddemo/SpringbootSftpDownloadDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hendisantika.springbootsftpdownloaddemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSftpDownloadDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
