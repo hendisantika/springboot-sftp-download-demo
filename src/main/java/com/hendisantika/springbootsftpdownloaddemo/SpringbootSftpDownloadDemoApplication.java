77a293f0b7307276d172c80f1ac59c9ed54fa614
hendisantika <hendisantika@yahoo.co.id>
2019-02-28 07:17:30 +0700

    #4 - Enable @IntegrationComponentScan & @EnableIntegration

--- src/main/java/com/hendisantika/springbootsftpdownloaddemo/SpringbootSftpDownloadDemoApplication.java
+++ src/main/java/com/hendisantika/springbootsftpdownloaddemo/SpringbootSftpDownloadDemoApplication.java
@@ -2,8 +2,12 @@ package com.hendisantika.springbootsftpdownloaddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.config.EnableIntegration;
 
 @SpringBootApplication
+@IntegrationComponentScan
+@EnableIntegration
 public class SpringbootSftpDownloadDemoApplication {
 
     public static void main(String[] args) {
